376a00440147dce506530e657024e3f9a4787161
vtphatt2 <thinhphat544@gmail.com>
2024-12-15 13:39:38 +0700

    set up the project structure for frontend side

--- eduforces_frontend/src/main.tsx
+++ eduforces_frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './pages/Login/Login.tsx'
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
---
+++ eduforces_frontend/src/pages/Contest/Contest.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contest: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Contest!</h1>
+    </div>
+  );
+};
+
+export default Contest;
