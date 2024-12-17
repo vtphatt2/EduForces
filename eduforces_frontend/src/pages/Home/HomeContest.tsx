7e43858a83b718da63c5a67213c56ef2997f50fe
Phạm Phú Lợi <phamphuloi01@gmail.com>
2024-12-17 16:47:34 +0700

    Add button logic in HomeContest

--- eduforces_frontend/src/pages/Home/HomeContest.tsx
+++ eduforces_frontend/src/pages/Home/HomeContest.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { HomeContestProps } from "./Type";
 import Button from "../../components/Button.tsx";
 import styles from "./HomeContest.module.css";
 
 const HomeContest: React.FC<HomeContestProps> = ({ title, timestamp }) => {
+  const [isRegistered, setIsRegistered] = useState(false);
+
   const handleRegisterClick = () => {
-    // Implement the registration logic here
-    console.log("Registered for contest");
+    setIsRegistered((prev) => !prev); 
   };
 
   return (
     <div className={styles.homeContestContainer}>
       <section className={styles.contestItem}>
         <div className={styles.contestTitleWrapper}>
-          <span className={styles.contestTitle}>{title}</span>
+          <span
+            className={`${styles.contestTitle} ${
+              isRegistered ? styles.registeredContestTitle : ""
+            }`}
+          >
+            {title}
+          </span>
         </div>
         <div className={styles.contestTimestampWrapper}>
           <span className={styles.contestTimestamp}>{timestamp}</span>
@@ -22,7 +29,12 @@ const HomeContest: React.FC<HomeContestProps> = ({ title, timestamp }) => {
 
       <aside className={styles.sidebar}>
         <section className={styles.registerButtonWrapper}>
-          <Button label="Register" onClick={handleRegisterClick} />
+          <Button
+            label={isRegistered ? "Cancel" : "Register"}
+            onClick={handleRegisterClick}
+            color={isRegistered ? "#1E285F" : "white"}
+            backgroundColor={isRegistered ? "#F2F1FA" : "#5D5A88"}
+          />
         </section>
       </aside>
     </div>
