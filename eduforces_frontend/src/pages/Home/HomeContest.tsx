c177aeb43c3cbd4a95e1ee47806d9025f3f410e4
Phạm Phú Lợi <phamphuloi01@gmail.com>
2024-12-17 15:48:34 +0700

    HomePage Layout Complete

--- eduforces_frontend/src/pages/Home/HomeContest.tsx
+++ eduforces_frontend/src/pages/Home/HomeContest.tsx
@@ -10,16 +10,21 @@ const HomeContest: React.FC<HomeContestProps> = ({ title, timestamp }) => {
   };
 
   return (
-    <div className={styles.contestItem}>
-      <div className={styles.contestTitleWrapper}>
-        <span className={styles.contestTitle}>{title}</span>
-      </div>
-      <div className={styles.contestTimestampWrapper}>
-        <span className={styles.contestTimestamp}>{timestamp}</span>
-      </div>
-      <div className={styles.registerButtonWrapper}>
-        <Button label="Register" onClick={handleRegisterClick} />
-      </div>
+    <div className={styles.homeContestContainer}>
+      <section className={styles.contestItem}>
+        <div className={styles.contestTitleWrapper}>
+          <span className={styles.contestTitle}>{title}</span>
+        </div>
+        <div className={styles.contestTimestampWrapper}>
+          <span className={styles.contestTimestamp}>{timestamp}</span>
+        </div>
+      </section>
+
+      <aside className={styles.sidebar}>
+        <section className={styles.registerButtonWrapper}>
+          <Button label="Register" onClick={handleRegisterClick} />
+        </section>
+      </aside>
     </div>
   );
 };
