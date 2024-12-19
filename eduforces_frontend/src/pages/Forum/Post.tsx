bceed38703fd73be1d574036f616493648ec5f17
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2024-12-19 10:08:53 +0700

    add fetch data

--- eduforces_frontend/src/pages/Forum/Post.tsx
+++ eduforces_frontend/src/pages/Forum/Post.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./Forum.module.css";
 import { PostProps } from "./Type";
 
@@ -6,9 +7,18 @@ export const Post: React.FC<PostProps> = ({
   title,
   shortDescription,
   author,
+  id,
 }) => {
+  const navigate = useNavigate();
+  const handlePostClick = async () => {
+    try {
+      navigate(`/post?id=${id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <article className={styles.postContainer}>
+    <article className={styles.postContainer} onClick={handlePostClick}>
       <h2 className={styles.postTitle}>{title}</h2>
       <p className={styles.postDescription}>{shortDescription}</p>
       <p className={styles.postAuthor}>
