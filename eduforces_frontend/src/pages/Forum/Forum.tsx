8759bd2b3442bce6087544392b8b45b90412c215
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2024-12-28 07:14:15 +0700

    change align of forum page

--- eduforces_frontend/src/pages/Forum/Forum.tsx
+++ eduforces_frontend/src/pages/Forum/Forum.tsx
@@ -36,52 +36,16 @@ const Forum: React.FC = () => {
 
       const data = await response.json();
       setPostList(data.data);
-      setNumPages(Math.ceil(data.meta.total / 10));
+      setNumPages(Math.ceil(data.meta.total / 5));
     } catch (error) {
       alert("Error: " + error);
     }
   };
 
   useEffect(() => {
-    getPostList(currentPage);
+    getPostList(currentPage, 5);
   }, [currentPage]);
 
-  // const postList = [
-  //   {
-  //     title: "Hard inequality",
-  //     shortDescription: "Let a + b = 1. Prove that...",
-  //     author: "congthanh1203",
-  //     id: "h4578t9h495u8ujrre8",
-  //   },
-  //   {
-  //     title: "Why is mathematics important in real life?",
-  //     shortDescription:
-  //       "Mathematics is integral to real life because it provides tools and frameworks for solving problems, making decisions, and understanding the world around us. Here are key reasons why mathematics is essential in everyday life:",
-  //     author: "phuloi1512",
-  //     id: "5478yt894t9ht9th9r8",
-  //   },
-  //   {
-  //     title: "Projectile Motion",
-  //     shortDescription:
-  //       "A ball is thrown with an initial velocity of 20 m/s at an angle of 30∘ to the horizontal. Calculate:",
-  //     author: "congthanh1203",
-  //     id: "fjh489jt8jj93tj4308tj",
-  //   },
-  //   {
-  //     title: "Free Fall",
-  //     shortDescription:
-  //       "A stone is dropped from the top of a 45-meter-high building. Calculate:",
-  //     author: "hoangky1802",
-  //     id: "lsmjfklojf849j8j9j8",
-  //   },
-  //   {
-  //     title: "Atomic Structure",
-  //     shortDescription:
-  //       "Write the electron configuration for the following elements:",
-  //     author: "thinhphat544",
-  //     id: "f8j4w9jf88jjfg0439j",
-  //   },
-  // ];
   const uploadPost = async () => {
     const title = (
       document.getElementById("titleTextArea") as HTMLTextAreaElement
@@ -126,14 +90,18 @@ const Forum: React.FC = () => {
         <div className={styles.forumContainer}>
           <h1 className={styles.forumTitle}>Posts</h1>
           <section className={styles.postList}>
-            {postList.map((post) => (
-              <Post
-                title={post.title}
-                shortDescription={post.content}
-                author={post.author_id}
-                id={post.post_id}
-              />
-            ))}
+            {postList === null ? (
+              <p style={{ color: "black" }}>No posts found</p>
+            ) : (
+              postList.map((post) => (
+                <Post
+                  title={post.title}
+                  shortDescription={post.content}
+                  author={post.author_id}
+                  id={post.post_id}
+                />
+              ))
+            )}
           </section>
           <NavPage numPages={numPages} />
         </div>
